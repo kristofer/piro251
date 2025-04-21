b3c7f7d3a07151f1de7a0155acf80adde888a3fe
Kristofer <kris@youngers.org>
2025-04-21 13:44:57 -0400

    init start.spring.io

---
+++ piro/src/main/java/rocks/zipcode/piro/PiroApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.piro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiroApplication.class, args);
+	}
+
+}
